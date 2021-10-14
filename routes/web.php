e76edf1204fdd56438013b77a1edc29fbdcb3cf1
OsmanBytyqi <obytyqi2001@gmail.com>
2021-10-14 22:13:04 +0200

    adding controller

--- routes/web.php
+++ routes/web.php
@@ -14,25 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/pizzas', function () {
-    // get data from a database
-    $pizzas = [
-      ['type' => 'hawaiian', 'base' => 'cheesy crust'],
-      ['type' => 'volcano', 'base' => 'garlic crust'],
-      ['type' => 'veg supreme', 'base' => 'thin & crispy']
-    ];
-    return view('pizzas', ['pizzas' => $pizzas,
-    'name'=>request('name'),
-    'age'=>request('age')
-    ]
-  );
-  });
+Route::get('/pizzas', 'PizzaController@index');
 
 
-  Route::get('/details/{id}',function($id){
-
-    return view('details',['id'=>$id]);
-
-
-
-  });
+  Route::get('/details/{id}','PizzaController@show');
