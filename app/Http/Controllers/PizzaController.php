f944b023b13017167aa727dc1dd0b08472ff2a13
OsmanBytyqi <obytyqi2001@gmail.com>
2021-10-20 00:53:12 +0200

    mvc

--- app/Http/Controllers/PizzaController.php
+++ app/Http/Controllers/PizzaController.php
@@ -7,18 +7,26 @@ use App\Pizza;
 
 class PizzaController extends Controller
 {
-    public function index(){
 
-        // $pizzas=Pizza::all();
-       // $pizzas=Pizza::orderby('name','desc')->get();
-            $pizzas=Pizza::where('type','hawaiian')->get();
-        return view('pizzas',['pizzas'=>$pizzas]);
-       
-       
-    }
+  public function index() {
 
-    public function show($id){
-        return view('details',['id'=>$id]);
+    // $pizzas = Pizza::all();  
+    // $pizzas = Pizza::orderBy('name', 'desc')->get();
+    // $pizzas = Pizza::where('type', 'hawaiian')->get();
+    $pizzas = Pizza::latest()->get();      
 
-    }
+    return view('pizzas.index', [
+      'pizzas' => $pizzas,
+    ]);
+  }
+
+  public function show($id) {
+    // use the $id variable to query the db for a record
+    return view('pizzas.show', ['id' => $id]);
+  }
+
+  public function create() {
+    return view('pizzas.create');
+  }
+
 }
